0995cd974986ef1c5374e14c2a6b0601cfac747e
Gokul G <gokulgunak@gmail.com>
2025-05-26 16:36:24 +0530

    git init

---
+++ src/main/java/com/gsk/encryptomate/EncryptomateApplication.java
@@ -0,0 +1,13 @@
+package com.gsk.encryptomate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptomateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncryptomateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsk/encryptomate/EncryptomateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsk.encryptomate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptomateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
